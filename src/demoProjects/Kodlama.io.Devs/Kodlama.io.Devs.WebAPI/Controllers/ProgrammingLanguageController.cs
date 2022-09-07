ee4040574b970fbade533e6370f07ebe6d03e669
Tahir Yıldırım <tahiryildirim19@gmail.com>
2022-09-07 21:18:57 +0300

    complete project

--- src/demoProjects/Kodlama.io.Devs/Kodlama.io.Devs.WebAPI/Controllers/ProgrammingLanguageController.cs
+++ src/demoProjects/Kodlama.io.Devs/Kodlama.io.Devs.WebAPI/Controllers/ProgrammingLanguageController.cs
@@ -1,20 +1,57 @@
-﻿using MediatR;
+﻿using Core.Application.Requests;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguage;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Commands.DeleteProgrammingLanguage;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Commands.UpdateProgrammingLanguage;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Dtos;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Models;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Queries.GetByIdProgrammingLanguage;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Queries.GetListProgrammingLanguage;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Kodlama.io.Devs.WebAPI.Controllers
 {
+
     [Route("api/[controller]")]
     [ApiController]
-    public class ProgrammingLanguageController : ControllerBase
+    public class ProgrammingLanguagesController : BaseController
     {
-
         [HttpPost]
-        public async Task<IActionResult> Add([FromBody] CreateProgrammingLanguageCommand createProgrammingLanguageCommand)
+        public async Task<IActionResult> Add([FromBody] CreateProgrammingLanguageCommand createProgramingLanguageCommand)
         {
-            CreatedProgrammingLanguageDto result = await Mediator.Send(createProgrammingLanguageCommand);
+            CreatedProgrammingLanguageDto result = await Mediator.Send(createProgramingLanguageCommand);
             return Created("", result);
         }
 
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateProgrammingLanguageCommand updateProgrammingLanguageCommand)
+        {
+            UpdatedProgrammingLanguageDto result = await Mediator.Send(updateProgrammingLanguageCommand);
+            return Created("", result);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody] DeleteByIdProgrammingLanguageCommand deleteByIdProgrammingLanguageCommand)
+        {
+            bool result = await Mediator.Send(deleteByIdProgrammingLanguageCommand);
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListProgrammingLanguageQuery getListProgrammingLanguageQuery = new() { PageRequest = pageRequest };
+            ProgrammingLanguageListModel result = await Mediator.Send(getListProgrammingLanguageQuery);
+
+            return Ok(result);
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetById([FromRoute] GetByIdProgrammingLanguageQuery getByIdProgrammingLanguageQuery)
+        {
+            ProgrammingLanguageGetByIdDto result = await Mediator.Send(getByIdProgrammingLanguageQuery);
+            return Ok(result);
+        }
     }
 }
